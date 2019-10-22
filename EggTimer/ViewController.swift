35c9d40ff6191af8bbf92e89ff3698c39e564050
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2019-10-22 15:31:59 +0200

    Changed titleLabel to done when countdown ends

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,51 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var counter = 0
+    @IBOutlet weak var titleLabel: UILabel!
+    var secondsRemaining = 60
     
-    let eggTimes = ["Soft":300, "Medium":420, "Hard":720]
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
-//    let softTime = 5
-//    let mediumTime = 7
-//    let hardTime = 12
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
+        timer.invalidate()
+        
         let hardness = sender.currentTitle!
         
-        let result = eggTimes[hardness]!
+        secondsRemaining = eggTimes[hardness]!
         
-        counter = result
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
 
-        print(result)
-        
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-        
-//        switch hardness {
-//        case "Soft":
-//            print(softTime)
-//        case "Medium":
-//            print(mediumTime)
-//        case "Hard":
-//                print(hardTime)
-//        default:
-//            print("Error")
-//        }
-        
-//        if hardness == "Soft" {
-//            print(softTime)
-//        } else if hardness == "Medium" {
-//            print(mediumTime)
-//        } else {
-//            print(hardTime)
-//        }
     }
     
-    @objc func updateCounter() {
+    @objc func updateTimer() {
         //example functionality
-        if counter > 0 {
-            print("\(counter) seconds.")
-            counter -= 1
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
         }
     }
 
