e315b5e13203924793902d391ada40d153dfa639
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2019-10-22 16:05:52 +0200

    added progressbar and sound after countdown finish

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,37 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var titleLabel: UILabel!
-    var secondsRemaining = 60
-    
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
-    
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var audioPlayer: AVAudioPlayer!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         timer.invalidate()
-        
         let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
         
-        secondsRemaining = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
 
     }
     
     @objc func updateTimer() {
-        //example functionality
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            print("\(totalTime-secondsPassed) seconds.")
         } else {
             timer.invalidate()
             titleLabel.text = "DONE!"
+            playSound()
         }
     }
-
+    
+    func playSound() {
+        let soundURL = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            } catch let error {
+                print(error.localizedDescription)
+        }
+        audioPlayer.play()
+    }
 }
